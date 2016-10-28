9b139bff9dd254ba505d7213d077cefa69d2217f
quezlatch <quezlatch@gmail.com>
2016-10-29 00:00:50 +0100

    first bit of recursion

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,30 @@
 var assert = require('assert');
 
+var isEqual = 0;
+var isLower = 1;
+var isHigher = 2;
+
 function karate(size, comparer) {
-    return 5;
+    function goHigher(current, last){
+        var midpoint = current + Math.floor((last - current)/2);
+        var c = comparer(midpoint)
+        if (c === isHigher) return goHigher(midpoint, last);
+        return midpoint
+    }
+    return goHigher(0, size);
 }
 
 describe('a sequence of 10', () => {
-    it('finds 5, as first midpoint', () => assert.equal(5, karate(10, ()=> 0)));
+    function makeComparer(i){
+        return function(s) {
+            if (s > i)
+                return isLower;
+            if (s < i)
+                return isHigher;
+            return isEqual;
+        }
+    }
+    var assertFindsNumberInSequence = i => assert.equal(i, karate(10, makeComparer(i)));
+    it('finds 5, as first midpoint', () => assertFindsNumberInSequence(5));
+    it('finds 7, as 2nd midpoint', () => assertFindsNumberInSequence(7));
 });
