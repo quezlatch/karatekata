f3cbcaf480d02d0ec924fc9dd70e10d7f7d51b4c
quezlatch <quezlatch@gmail.com>
2016-10-29 22:14:14 +0100

    tidy up the tests

--- test/tests.js
+++ test/tests.js
@@ -20,30 +20,23 @@ function karate(size, comparer) {
 }
 
 describe('a sequence of 10', () => {
-    function makeComparer(i){
+    function makeComparer(i) {
         return function(s) {
-            if (s > i)
-                return isLower;
-            if (s < i)
-                return isHigher;
+            if (s > i) return isLower;
+            if (s < i) return isHigher;
             return isEqual;
         }
     }
-    var assertFindsNumberInSequence = i => assert.equal(i, karate(10, makeComparer(i)));
     describe('a sequence of 10', () => {
-        describe('going up', () => {
-            it('finds 5, as first midpoint', () => assertFindsNumberInSequence(5));
-            it('finds 7, as 2nd midpoint', () => assertFindsNumberInSequence(7));
-            it('finds 8, as 3rd midpoint', () => assertFindsNumberInSequence(8));
-            it('finds 9, as 4th midpoint', () => assertFindsNumberInSequence(9));
-            it('has not found, as goes out of bounds', () => assert.equal(notFound, karate(10, () => isHigher)));
+        describe('for every number in the sequence', () => {
+            for (var i = 0; i < 10; i++) {
+                var item = i;
+                it('finds number ' + i, () => assert.equal(item, karate(10, makeComparer(item))));
+            }
         });
-        describe('going down', () => {
-            it('finds 3, as 2nd midpoint', () => assertFindsNumberInSequence(3));
-            it('finds 2, as 2nd midpoint', () => assertFindsNumberInSequence(3));
-            it('finds 1, as 2nd midpoint', () => assertFindsNumberInSequence(3));
-            it('finds 0, as 2nd midpoint', () => assertFindsNumberInSequence(3));
-            it('has not found, as goes out of bounds', () => assert.equal(notFound, karate(10, () => isLower)));
+        describe('cannot find', () => {
+            it('a number after the sequence', () => assert.equal(notFound, karate(10, () => isHigher)));
+            it('a number before the sequence', () => assert.equal(notFound, karate(10, () => isLower)));
         });
     });
 });
